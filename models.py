7637372082e9683cfa63e881f281540d2e16ba6b
2509494@leedstrinity.ac.uk <VICTOR@VICTORS-MACBOOK-PRO.local>
2025-12-02 18:00:54 +0000

    implement models.py for the database

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(10), nullable=False, default='patient')
